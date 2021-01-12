89d1845631afba017d1d0e2c40c22d3fed95823c
arun-anjilik <52011992+arun-anjilik@users.noreply.github.com>
2021-01-12 14:38:16 +0530

    Update auto_forward.py

--- forwarder/modules/auto_forward.py
+++ forwarder/modules/auto_forward.py
@@ -15,8 +15,8 @@ def forward(bot: Bot, update: Update):
     for chat in TO_CHATS:
         to_chat_name = bot.get_chat(chat).title or bot.get_chat(chat).first_name
         try:
-	    bot.forward_message(chat_id=chat, from_chat_id=from_chat_id, message_id=message.message_id)
-	
+            bot.forward_message(chat_id=chat, from_chat_id=from_chat_id, message_id=message.message_id)
+        
         except:
             LOGGER.exception("Error while forwarding message from chat \"{}\" to chat \"{}\".".\
                              format(from_chat_name, to_chat_name))
