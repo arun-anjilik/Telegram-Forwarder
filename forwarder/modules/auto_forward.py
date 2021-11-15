c039a76496c3387f097bad209ff31328d438cbc5
arun-anjilik <52011992+arun-anjilik@users.noreply.github.com>
2021-11-15 11:30:13 +0530

    Update auto_forward.py

--- forwarder/modules/auto_forward.py
+++ forwarder/modules/auto_forward.py
@@ -22,21 +22,6 @@ def forward(bot: Bot, update: Update):
             LOGGER.exception("Error while forwarding message from chat \"{}\" to chat \"{}\".".\
                              format(from_chat_name, to_chat_name))
 
-    if (update.effective_message.gif):
-        message = update.effective_message  # type: Optional[Message]
-
-    from_chat_id = update.effective_chat.id
-    from_chat_name = update.effective_chat.title or update.effective_chat.first_name
-    
-    for chat in GIF_CHATS:
-        to_chat_name = bot.get_chat(chat).title or bot.get_chat(chat).first_name
-        try:
-            bot.forward_message(chat_id=chat, from_chat_id=from_chat_id, message_id=message.message_id)
-        
-        except:
-            LOGGER.exception("Error while forwarding message from chat \"{}\" to chat \"{}\".".\
-                             format(from_chat_name, to_chat_name))
-
 
 try:
     FORWARD_HANDLER = MessageHandler(Filters.chat(FROM_CHATS) & ~Filters.status_update & ~Filters.command,
