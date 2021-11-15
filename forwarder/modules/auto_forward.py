8f5d9d451614d7b57f5566c01ec490ffe207db39
arun-anjilik <52011992+arun-anjilik@users.noreply.github.com>
2021-11-15 11:19:07 +0530

    Update auto_forward.py

--- forwarder/modules/auto_forward.py
+++ forwarder/modules/auto_forward.py
@@ -9,12 +9,26 @@ from forwarder import FROM_CHATS, TO_CHATS, GIF_CHATS, LOGGER, dispatcher
 def forward(bot: Bot, update: Update):
     if not (update.effective_message.text):
         message = update.effective_message  # type: Optional[Message]
-        if (message.gif):
-             TO_CHATS = GIF_CHATS
+       
     from_chat_id = update.effective_chat.id
     from_chat_name = update.effective_chat.title or update.effective_chat.first_name
     
     for chat in TO_CHATS:
+        to_chat_name = bot.get_chat(chat).title or bot.get_chat(chat).first_name
+        try:
+            bot.forward_message(chat_id=chat, from_chat_id=from_chat_id, message_id=message.message_id)
+        
+        except:
+            LOGGER.exception("Error while forwarding message from chat \"{}\" to chat \"{}\".".\
+                             format(from_chat_name, to_chat_name))
+
+    if (update.effective_message.gif):
+        message = update.effective_message  # type: Optional[Message]
+
+    from_chat_id = update.effective_chat.id
+    from_chat_name = update.effective_chat.title or update.effective_chat.first_name
+    
+    for chat in GIF_CHATS:
         to_chat_name = bot.get_chat(chat).title or bot.get_chat(chat).first_name
         try:
             bot.forward_message(chat_id=chat, from_chat_id=from_chat_id, message_id=message.message_id)
