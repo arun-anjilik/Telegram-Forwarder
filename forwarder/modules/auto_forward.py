1b95c9103dad43d8111fb13165b19f98729b5611
arun-anjilik <52011992+arun-anjilik@users.noreply.github.com>
2021-12-13 22:17:16 +0530

    Update auto_forward.py

--- forwarder/modules/auto_forward.py
+++ forwarder/modules/auto_forward.py
@@ -17,7 +17,7 @@ def forward(bot: Bot, update: Update):
         # arr=[]
         # arr.append(mid)
         l.put(mid)
-        bot.send_message(chat_id=from_chat_id, mid)
+        bot.send_message(chat_id=from_chat_id, message=mid)
         if (l.full()):
                # i = 0  
                # rmid = arr[0]
