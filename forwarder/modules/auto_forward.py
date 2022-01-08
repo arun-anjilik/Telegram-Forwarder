406ca0b8862a2dda4cee175b1d078e9459f3a303
arun-anjilik <52011992+arun-anjilik@users.noreply.github.com>
2022-01-08 13:18:26 +0530

    Update auto_forward.py

--- forwarder/modules/auto_forward.py
+++ forwarder/modules/auto_forward.py
@@ -2,37 +2,26 @@ from telegram import Bot, Update
 from telegram.ext import MessageHandler, Filters
 from telegram.ext.dispatcher import run_async
 import time
-import queue
 
 from forwarder import FROM_CHATS, TO_CHATS, GIF_CHATS, LOGGER, dispatcher
-l = queue.Queue(maxsize=20)
+
 @run_async
 def forward(bot: Bot, update: Update):
-  if not (update.effective_message.text):
+    if not (update.effective_message.text):
         message = update.effective_message  # type: Optional[Message]
-        
+       
         from_chat_id = update.effective_chat.id
         from_chat_name = update.effective_chat.title or update.effective_chat.first_name
-        mid = message.message_id
-        # arr=[]
-        # arr.append(mid)
-        
-        # bot.send_message(chat_id=from_chat_id, text=mid)
-        try:
-          if not(l.full()):
-               # i = 0  
-               # rmid = arr[0]
-              l.put(mid)
-             # for i in range(50):
-               #      arr[i] = arr[i+1]
-               # arr.append(mid)
-          else:
-            rmid = l.get()
-            bot.delete_message(chat_id=from_chat_id, message_id=rmid)
-        except:
-                LOGGER.exception("Error while forwarding message from chat \"{}\" to chat \"\".".\
-                             format(from_chat_name))
-           
+    
+        for chat in TO_CHATS:
+            to_chat_name = bot.get_chat(chat).title or bot.get_chat(chat).first_name
+            try:
+#                bot.forward_message(chat_id=chat, from_chat_id=from_chat_id, message_id=message.message_id)
+                 time.sleep(10)
+                 bot.delete_message(chat_id=from_chat_id, message_id=message.message_id)
+            except:
+                LOGGER.exception("Error while forwarding message from chat \"{}\" to chat \"{}\".".\
+                             format(from_chat_name, to_chat_name))
         
 
 
